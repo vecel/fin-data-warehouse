2261e30124dda08bf827dd68e748316be48af67c
lucknera <01180883@pw.edu.pl>
2026-06-02 14:54:30 +0200

    Moved fred_loader to fetchers

--- data-acquisition/src/fetchers/macro.py
+++ data-acquisition/src/fetchers/macro.py
@@ -1,9 +1,12 @@
+import os
+import logging
 import pandas as pd
 from fredapi import Fred
 
 # TODO: Move to fetchers, add job, schedule job, load .env from project root
 
-# Indexes to gather
+logger = logging.getLogger(__name__)
+
 FRED_SERIES_MAP = {
     'US_GDP': 'GDP',
     'PL_GDP': 'CPMNACSCAB1GQPL',
@@ -15,7 +18,13 @@ FRED_SERIES_MAP = {
     'PL_INTEREST': 'IR3TIB01PLM156N'
 }
 
-def load_fred_data(api_key: str) -> pd.DataFrame:
+def fetch_macro_data() -> pd.DataFrame:
+    logger.info('Fetching macro data from FRED.')
+    api_key = os.getenv("FRED_API_KEY")
+    if not api_key:
+        logger.error("Missing FRED_API_KEY environment variable!")
+        raise ValueError("Missing FRED_API_KEY")
+
     fred = Fred(api_key=api_key)
     all_data = []
 
@@ -25,16 +34,17 @@ def load_fred_data(api_key: str) -> pd.DataFrame:
             df = series.reset_index()
             df.columns = ['date', 'indicator_value']
             df['indicator_code'] = code
-            df['indicator_name_custom'] = name
+            df['indicator_name'] = name
+            df['country_code'] = 'PL' if name.startswith('PL') else 'US'
             
             all_data.append(df)
         except Exception as e:
-            print(f"Error loading code {code}: {e}")
+            logger.error(f"Error loading code {code}: {e}")
 
     if all_data:
         final_df = pd.concat(all_data, ignore_index=True)
         final_df = final_df.dropna(subset=['indicator_value'])
         return final_df
     
-    print("No data found!")
+    logger.warning("No macro data found!")
     return pd.DataFrame()
