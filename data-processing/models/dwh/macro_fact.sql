bb5d5a33ad98885b475144e5da1ba0cc10bbb475
lucknera <01180883@pw.edu.pl>
2026-06-10 14:50:37 +0200

    Added macro fact table

---
+++ data-processing/models/dwh/macro_fact.sql
@@ -0,0 +1,28 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH macro_raw AS (
+    SELECT
+        date AS macro_date,
+        indicator_value,
+        indicator_code,
+        country_code
+    FROM raw.macro
+),
+
+macro_fact AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['m.indicator_code', 'm.macro_date', 'm.country_code']) }} AS macro_fact_id,
+        
+        {{ to_date_id('m.macro_date') }} AS date_id,
+        c.country_id,
+        i.indicator_id,
+        m.indicator_value::DOUBLE PRECISION AS indicator_value
+    FROM macro_raw m
+    LEFT JOIN {{ ref('country_dim') }} c ON m.country_code = c.country_code
+    LEFT JOIN {{ ref('macro_indicator_dim') }} i ON m.indicator_code = i.indicator_code
+    WHERE c.country_id IS NOT NULL AND i.indicator_id IS NOT NULL
+)
+
+SELECT * FROM macro_fact
