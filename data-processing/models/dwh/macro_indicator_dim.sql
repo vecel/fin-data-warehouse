d29b6f37c554cc2aa94786189c6d3c222fbeb3ae
lucknera <01180883@pw.edu.pl>
2026-06-10 14:49:01 +0200

    Added macro indicator dim

---
+++ data-processing/models/dwh/macro_indicator_dim.sql
@@ -0,0 +1,22 @@
+{{ config(
+    unique_key='indicator_id'
+) }}
+
+WITH indicators AS (
+    SELECT DISTINCT
+        indicator_code::VARCHAR(20) AS indicator_code,
+        indicator_name::VARCHAR(100) AS indicator_name,
+        'Monthly/Quarterly'::VARCHAR(20) AS frequency
+    FROM raw.macro
+),
+
+macro_indicator_dim AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['indicator_code']) }} AS indicator_id,
+        indicator_code,
+        indicator_name,
+        frequency
+    FROM indicators
+)
+
+SELECT * FROM macro_indicator_dim
